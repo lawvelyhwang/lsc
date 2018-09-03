1876eedf91cf9ec6dcc9df73395c08dcf2454299
gomguard <34765216+gomguard@users.noreply.github.com>
2018-09-03 17:05:40 +0900

    jdj update

---
+++ reserve_detail_jdj.php
@@ -0,0 +1,90 @@
+<?php
+  include("head.php");
+
+?><div class="" align="center">
+  <h2>자기소개서 예약 페이지 - 정동주 변호사</h2>
+  <br>
+  * 정동주 변호사 자기소개서 첨삭은 강남에서만 진행합니다.
+  <br>
+  <br>
+
+    <div class="input-group" style="width: 80%;">
+
+  <br>
+<div class="" align="center">
+  <table class="table" style="width: 80%; text-align: center;">
+    <tr>
+      <td>시간</td>
+
+      <td>9/8(토)</td>
+      <td>9/10(월)</td>
+      <td>9/15(토)</td>
+      <td>9/17(월)</td>
+      <td>9/22(토)</td>
+    </tr>
+    <?php
+      $sql = "select a.time as time, a.id as id_1, a.loc as loc_1, b.id as id_2, b.loc as loc_2,
+  c.id as id_3, c.loc as loc_3, d.id as id_4, d.loc as loc_4, e.id as id_5, e.loc as loc_5
+    from (select * from reserve_essay_jdj where week = 1 and day = 1) as a
+  left join (select * from reserve_essay_jdj where week = 1 and day = 2) as b on a.time = b.time
+  left join (select * from reserve_essay_jdj where week = 1 and day = 3) as c on a.time = c.time
+  left join (select * from reserve_essay_jdj where week = 1 and day = 4) as d on a.time = d.time
+  left join (select * from reserve_essay_jdj where week = 1 and day = 5) as e on a.time = e.time;";
+      $result = mysqli_query($db, $sql);
+
+
+        while($row = mysqli_fetch_assoc($result)){
+          echo "<tr>";
+          # 시간 채우기
+          echo "<td>{$row['time']}</td>";
+          # 요일 채우기
+          for ($i=1; $i <= 5 ; $i++) {
+            $loc = "";
+            $id = "";
+            switch ($i) {
+              case 1:
+                $loc = $row['loc_1'];
+                $id = $row['id_1'];
+                break;
+              case 2:
+                $loc = $row['loc_2'];
+                $id = $row['id_2'];
+                break;
+              case 3:
+                $loc = $row['loc_3'];
+                $id = $row['id_3'];
+                break;
+              case 4:
+                $loc = $row['loc_4'];
+                $id = $row['id_4'];
+                break;
+              case 5:
+                $loc = $row['loc_5'];
+                $id = $row['id_5'];
+                break;
+              }
+
+              if ($loc == "강남") {
+                $_bgcol = "#fdf8d6";
+              }else{
+                $_bgcol = "#ffeeee";
+              }
+
+            echo "<td style=\"background : {$_bgcol};\">";
+            if ($id != "") {
+              echo "예약중";
+            }else{
+              echo "<a class=\"btn btn-default\" href =\"./reserve_process_jdj.php?week=1&day={$i}&step={$_GET['step']}&time={$row['time']}\">예약</a>";
+            }
+            echo "</td>";
+          }
+        echo "</tr>";
+      }
+
+    ?>
+  </table>
+</div>
+
+<?php
+  include("foot.php");
+ ?>
---
+++ reserve_process_jdj.php
@@ -0,0 +1,45 @@
+<?php include("config.php"); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+
+ <meta http-equiv='refresh' content='0;url=reserve.php'>
+
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    session_start();
+    #$_SESSION['login_user']
+    # 사용할 변수 week, day, step, time
+
+    # 현재시간이 가능한지
+    $sql_tf = "select * from reserve_essay_jdj where week = {$_GET['week']} and day = {$_GET['day']} and time = \"{$_GET['time']}\";";
+    $result_tf = mysqli_query($db, $sql);
+    $row = mysqli_fetch_assoc($result_tf);
+    if ($row['id'] == "") {
+
+    #예약한 상태인지 아닌지 먼저 확인 - step, id
+
+      $sql = "select * from reserve_essay_jdj where id = \"{$_SESSION['login_user']}\" and step = {$_GET['step']};";
+
+      $result = mysqli_query($db, $sql);
+      $row_tf = mysqli_num_rows($result);
+      if ($row_tf > 0 ) {
+        $sql_upt = "update reserve_essay_jdj set id = \"\", step = \"\" where id = \"{$_SESSION['login_user']}\" and step = {$_GET['step']};";
+        $result_upt = mysqli_query($db, $sql_upt);
+      }
+
+      $sql_upt = "update reserve_essay_jdj set id = \"{$_SESSION['login_user']}\", step = {$_GET['step']} where week = {$_GET['week']} and day = {$_GET['day']} and time = \"{$_GET['time']}\";";
+      $result = mysqli_query($db, $sql_upt);
+    }else{ # 현재 시간이 차있다면
+      echo "<script>alert('이미 예약 된 시간 입니다. 다른 시간에 예약해 주십시오.');history.back();</script>";
+      exit;
+    }
+
+    ?>
+
+  </body>
+</html>
